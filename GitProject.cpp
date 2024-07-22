a7dfdab22dcb6794306d2f493c019ac91e004ab9
fatima Bawzir <fatimabawzir757@gmail.com>
2024-07-22 14:48:46 +0300

    function3

---
+++ GitProject.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+
+//function1
+void Start() {
+	cout << "Hello I am practice git&github";
+}
+//function2
+int sumOfTwoNumbers(int num1, int num2) {
+	return num1 + num2;
+}
+//function 3
+int multiplictionOfTwoNumbers(int num1,int num2) {
+	
+	return num1 * num2;
+}
+int main() {
+	Start();
+	cout << sumOfTwoNumbers(10,20);
+	cout << multiplictionOfTwoNumbers(4,5);
+}
